894159c1b3f7f0c6729d53ed1ed066d050d4a616
Benjamin <muhlestein1@gmail.com>
2022-06-07 09:16:20 -0700

    added code

--- app.js
+++ app.js
@@ -1,78 +1,52 @@
 // map object
+const myMap = {
+	coordinates: [],
+	businesses: [],
+	map: {},
+	markers: {},
 
-// get coordinates via geolocation api
-
-// get foursquare businesses
-
-// process foursquare array
-
-// event handlers
-// window load
-
-// business submit button
-
-
-// Create map:                                                       
-const myMap = L.map('map', {
-    center: [48.868672, 2.342130],
-    zoom: 12,
-});
-var map = L.map('map').setView([51.505, -0.09], 13);
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    maxZoom: 19,
-    attribution: '© OpenStreetMap'
-}).addTo(map);
-
-
-
-
-// Create and add a geolocation marker:
-const marker = L.marker([48.87007, 2.346453])
-marker.addTo(myMap).bindPopup('<p1><b>The Hoxton, Paris</b></p1>').openPopup()
-
-// map object 
-let mymap = {
-	coordinates: [36.5388869],
-	buuesiness: [],
-	map: [],
-	markers: [], 
-
+	// build leaflet map
 	buildMap() {
-        this.map = L.map('map', {
-			center: this.coordinates,
-			zoom: 11
-
-
+		this.map = L.map('map', {
+		center: this.coordinates,
+		zoom: 11,
 		});
-
-    // add openstreetmap tiles
-    L.tileLayer('https://.tile.openstreetmap.org///.png', {
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-    minZoom: '15',
-     }).addTo(this.map);
-     let marker = L.marker(this.coordinates)
-	 .addTo(this.map).bindPopup('<p1><b>Your Location!</b></p1>').openPopup()
-
+		// add openstreetmap tiles
+		L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+		attribution:
+			'&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+		minZoom: '15',
+		}).addTo(this.map)
+		// create and add geolocation marker
+		const marker = L.marker(this.coordinates)
+		marker
+		.addTo(this.map)
+		.bindPopup('<p1><b>You are here</b><br></p1>')
+		.openPopup()
 	},
 
-	// get foursquare businesses
-
-
+	// add business markers
+	addMarkers() {
+		for (var i = 0; i < this.businesses.length; i++) {
+		this.markers = L.marker([
+			this.businesses[i].lat,
+			this.businesses[i].long,
+		])
+			.bindPopup(`<p1>${this.businesses[i].name}</p1>`)
+			.addTo(this.map)
+		}
+	},
 }
+
+// get coordinates via geolocation api
 async function getCoords(){
-    pos = await new Promise((resolve, reject) => {
-        navigator.geolocation.getCurrentPosition(resolve, reject)
-    })
-    return [pos.coords.latitude, pos.coords.longitude]
+	const pos = await new Promise((resolve, reject) => {
+		navigator.geolocation.getCurrentPosition(resolve, reject)
+	});
+	return [pos.coords.latitude, pos.coords.longitude]
 }
 
-
-window.onload = async () => {
-      let coords = await getCoords()
-	  myMap.coordinates = coords
-	  myMap.buildmap()
-
-}
+// get foursquare businesses
 async function getFoursquare(business) {
 	const options = {
 		method: 'GET',
@@ -90,3 +64,33 @@ async function getFoursquare(business) {
 	let businesses = parsedData.results
 	return businesses
 }
+// process foursquare array
+function processBusinesses(data) {
+	let businesses = data.map((element) => {
+		let location = {
+			name: element.name,
+			lat: element.geocodes.main.latitude,
+			long: element.geocodes.main.longitude
+		};
+		return location
+	})
+	return businesses
+}
+
+
+// event handlers
+// window load
+window.onload = async () => {
+	const coords = await getCoords()
+	myMap.coordinates = coords
+	myMap.buildMap()
+}
+
+// business submit button
+document.getElementById('submit').addEventListener('click', async (event) => {
+	event.preventDefault()
+	let business = document.getElementById('business').value
+	let data = await getFoursquare(business)
+	myMap.businesses = processBusinesses(data)
+	myMap.addMarkers()
+})
